73a72cf1c346bd4b2ae2017c0843467a6ebc62ae
MohamadHelli <mou.hamad123@hotmail.com>
2022-07-18 15:32:17 +0200

    db

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2022 at 11:54 AM
+-- Generation Time: Jul 18, 2022 at 03:31 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -35,17 +35,44 @@ CREATE TABLE `proudct` (
   `price` int(11) NOT NULL,
   `pic` varchar(100) NOT NULL,
   `dis` varchar(255) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `name` varchar(50) NOT NULL,
+  `status` enum('user','adm') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `proudct`
 --
 
-INSERT INTO `proudct` (`id`, `price`, `pic`, `dis`, `name`) VALUES
-(1, 7, 'pic1.jpg', 'It is a very delicious pizza', 'Pizza Mona'),
-(2, 8, 'pic2.jpg', 'ANother delious pizza', 'Pizza Mohamad'),
-(3, 9, 'pic3.jpg', 'it is just a pizza', 'pizza php');
+INSERT INTO `proudct` (`id`, `price`, `pic`, `dis`, `name`, `status`) VALUES
+(1, 13, 'pic1.jpg', 'It is a very delicious pizza ok', 'Pizza Mona', 'user'),
+(3, 9, 'pic3.jpg', 'it is just a pizza', 'pizza php', 'user'),
+(4, 50, '62d004cf15e7e.jpg', 'high quality Pizaa $$$$', 'Pizza M&M', 'user'),
+(5, 14, 'pic4.jpg', 'mona', 'mona', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `status` enum('user','adm') DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `fname`, `lname`, `email`, `date_of_birth`, `pass`, `picture`, `status`) VALUES
+(1, 'mohamad', 'helli', 'mohamad@gmail.com', '2013-06-27', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e', '62d55fa9596b7.jpg', 'user'),
+(2, 'mohamadd', 'hellii', 'mo@gmail.com', '2014-02-13', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e', 'admavatar.png', 'adm');
 
 --
 -- Indexes for dumped tables
@@ -58,6 +85,12 @@ ALTER TABLE `proudct`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,7 +98,13 @@ ALTER TABLE `proudct`
 -- AUTO_INCREMENT for table `proudct`
 --
 ALTER TABLE `proudct`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
